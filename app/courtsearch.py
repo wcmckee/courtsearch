2f782a79984962eda13e63da59e2849244f83004
William Mckee <hammersmake@gmail.com>
2018-03-24 13:47:30 +1100

    change format/save as search term

--- app/courtsearch.py
+++ app/courtsearch.py
@@ -32,9 +32,7 @@ Bootstrap(app)
 @app.route('/xml', methods=['GET', 'POST'])
 def query_example():
         if request.method == 'POST':  #this block is only entered when the form is submitted
-            countrycode = request.form.get('countrycode')
-            statecode = request.form.get('statecode')
-            courtcode = request.form.get('courtcode')
+            countrycode = request.form.get('porntag')
             #<input type="radio" name="results" value="10" checked> 10<br>
             vslist = list()
 
@@ -140,39 +138,23 @@ def query_example():
 @app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
 def form_example():
     if request.method == 'POST':  #this block is only entered when the form is submitted
-        searchtext = request.form.get('searchtext')
-        maskpath = request.form.get('maskpath')
-        fromdate = request.form.get('fromdate')
-        todate = request.form.get('todate')
-
-        ordget = request.form.get('order')
-        #<input type="radio" name="results" value="10" checked> 10<br>
-        resultspage = request.form.get('results')
-
-        getda = arrow.get(fromdate)
-        getdate = getda.datetime
-
-        getend = arrow.get(todate)
-        enddate = getend.datetime
-
-        rannum = random.randint(0,24)
-
-
+        searchtext = request.form.get('porntag')
 
 
         #reqgif = requests.get("https://api.giphy.com/v1/gifs/search?api_key=ee58ff1d10c54fd29ddb0388126c2bcd&q=drugs&limit=25&offset=0&rating=PG-13&lang=en")
         reqhif = requests.get('https://api.giphy.com/v1/gifs/search?api_key=ee58ff1d10c54fd29ddb0388126c2bcd&q={}&limit=25&offset=0&rating=PG-13&lang=en'.format(searchtext))
 
         reqxm = (reqhif.json())
+        rannum = random.randint(0,24)
 
-        gifresult = reqxm['data'][rannum]['images']['fixed_width']['url']
+        gifresult = reqxm['data'][0]['images']['fixed_width']['url']
 
         img = Image.open('/home/{}/dmgit/app/static/template.jpg'.format(myusr))
 
-        upzero = "I don't always search for {}".format(searchtext)
+        upzero = "I don't always watch porn"
 
 
-        botzero = "But when I do I use InfoTrack"
+        botzero = "But when I do I watch {}".format(searchtext)
 
         imageSize = img.size
 
@@ -208,7 +190,7 @@ def form_example():
         draw.text(topTextPosition, upzero, (255,255,255), font=font)
         draw.text(bottomTextPosition, botzero, (255,255,255), font=font)
 
-        img.save("/home/{}/dmgit/app/static/newlogo.jpg".format(myusr))
+        img.save("/home/{}/dmgit/app/static/{}.jpg".format(myusr, searchtext))
         #               album_path="me/photos")
 
 
@@ -216,31 +198,18 @@ def form_example():
 
 
 
-        reqsearch = requests.get("http://www.austlii.edu.au/cgi-bin/sinosrch.cgi?method=auto;query={};results={};view={}&year1={}&year2={}&mask_path={}".format(searchtext, resultspage, ordget, getdate.year,  enddate.year,  maskpath))
-
-
-        bstxt = bs4.BeautifulSoup(reqsearch.text)
-
-        sometxt = (bstxt.find_all('li', {'class' : 'multi'}))
-
-        #print(sometxt)
-
-        #for somet in sometxt:
-        #    print(somet.text)
-
 
         return '''
 
-                <img src="/static/newlogo.jpg"><br>
-                <h1>legal materials results</h1><br>
-                <p>{}</p>
+                <img src="/static/{}.jpg"><br>
+                <h1>porntag results</h1><br>
                 <img src="{}">
                 <h3>Results for<br>
                 {}</h3><br>
-                <br><p>{}</p>
 
 
-                  '''.format(getdate, gifresult, searchtext, sometxt)
+
+                  '''.format(searchtext, gifresult, searchtext)
 
     return '''
             <!DOCTYPE html>
@@ -249,10 +218,6 @@ def form_example():
         <head>
 
           <title>Search legal materials</title>
-          <script src="https://unpkg.com/vue"></script>
-          <script src="node_modules/vue/vue.min.js"></script>
-          <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap/dist/css/bootstrap.min.css"/>
-          <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css"/>
           <meta name="viewport" content="width=device-width, initial-scale=1">
           </head>
           <body>
@@ -262,24 +227,8 @@ def form_example():
             <form method="POST">
               <fieldset>
                 <legend>Search legal materials:</legend>
-                  keyword: <input type="text" name="searchtext"><br>
-                  maskpath : <input type="text" name="maskpath"><br>
-                  Select from date:<br>
-                  <input style="box-sizing:content-box;"class='calander' name="fromdate" type="date"><br>
-                  Select to date:<br>
-                  <input style="box-sizing:content-box;"class='calander' name="todate" type="date"><br>
-                  Sort:<br>
-                  <input type="radio" name="order" value="relevance" checked> By Relevance<br>
-                  <input type="radio" name="order" value="citation"> By Citation Frequency<br>
-                  <input type="radio" name="order" value="database"> By Database<br>
-                  <input type="radio" name="order" value="date"> By Date<br>
-                  <input Type="radio" name="order" value="title"> By Title<br>
-                  Results per page:<br>
-                  <input type="radio" name="results" value="10" checked> 10<br>
-                  <input type="radio" name="results" value="20"> 20<br>
-                  <input type="radio" name="results" value="50"> 50<br>
-                  <input type="radio" name="results" value="100"> 100<br>
-                  <input Type="radio" name="results" value="all"> Display all<br>
+                  porn tag: <input type="text" name="porntag"><br>
+
 
                   <input type="submit" value="Submit"><br>
                   </fieldset>
